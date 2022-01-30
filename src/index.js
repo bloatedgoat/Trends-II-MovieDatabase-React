248a9a68608a40dc405d73e13ad66921ad3c62cd
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-01-30 15:02:12 +0100

    last fix for detail page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.module.css'
 import Home from './views/home'
 import Movies from './views/movies'
