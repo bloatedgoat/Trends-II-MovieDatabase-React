fdb0e46a323b03828173ab1cbcf9af0c76f49221
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-01-30 14:58:53 +0100

    detail fixed

--- src/views/selected-movie-serie.js
+++ src/views/selected-movie-serie.js
@@ -9,13 +9,17 @@ import {useParams} from 'react-router-dom'
 const SelectedMovieSerie = () => {
   const {id} = useParams();
   const [movie,setMovie] = useState([])
-
+  const [trailer, setTrailer] = useState([])
   useEffect(()=>{
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=07a61de5b731a869bc9cec8e25d2c8a8`)
     .then(res=>res.json())
     .then(data=>{
       setMovie(data)
     })
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=07a61de5b731a869bc9cec8e25d2c8a8`)
+      .then(res => res.json())
+      .then(data => setTrailer(data.results[0]))
   },[])
 
   return (
@@ -27,33 +31,38 @@ const SelectedMovieSerie = () => {
           content="SelectedMovie/Serie - Trends II MovieDatabase"
         />
       </Helmet>
-      <Header rootClassName="rootClassName1"></Header>
-      <h1 className={styles['Titel']}>{movie.original_title}</h1>
       <div className={styles['container1']}>
         <img
           alt="image"
-          src="https://play.teleporthq.io/static/svg/default-img.svg"
+          src={`https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
           className={styles['Poster']}
         />
-        <video
-          src
-          poster="https://play.teleporthq.io/static/svg/videoposter.svg"
-          className={styles['Trailer']}
-        ></video>
+        {
+          trailer.length === 0 ?
+            ''
+            :
+            <iframe width="560" height="315" className={styles['Trailer']} src={`https://www.youtube.com/embed/${trailer.key}`} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        }
       </div>
-      <div className={styles['container2']}>
-        <span className={styles['Score']}>
-          <span>SCORE</span>
-          <span>&amp;#8203;</span>
-        </span>
-        <div className={styles['MovieInformation']}>
-          <div className={styles['ContainerReleaseDate']}></div>
-          <div className={styles['ContainerGenre']}></div>
-          <div className={styles['ContainerDirectorActors']}></div>
-          <div className={styles['ContainerSynopsis']}></div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <span className='display-4 text-light'>SCORE {movie.vote_average}</span>
+          </div>
+          <div className="col-md-10">
+            <Badge bg="primary" className="m-1">Release Date: {movie.release_date}</Badge>
+            <Badge bg="primary" className="m-1">popularity: {movie.popularity}</Badge>
+            <Badge bg="primary" className="m-1">Lang: {movie.original_language}</Badge>
+
+            <p className="display-4 text-light">Overview:</p>
+            <p className="lead text-light">
+              {movie.overview}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      </div>
   )
 }
 
